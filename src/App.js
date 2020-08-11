d3fe18a39d91c13c2ab9b85b415b0b6a4d605fb8
Patil <rudragoud.patil@compassitesinc.com>
2020-08-11 16:11:47 +0530

    adds hot re-loading to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React  from "react";
 import "./App.css";
+import {hot} from "react-hot-loader"
 
 const App  = () => (
     <div class="App">
-        <h1>Welcome to React Scratch</h1>
+        <h1>Welcome to React Scratch!!!</h1>
     </div>
 );
 
-export default App;
+export default hot(module)(App);
