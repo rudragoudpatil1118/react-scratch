cce11ff6450a424146077cd715b194c8e9b3b5e6
Patil <rudragoud.patil@compassitesinc.com>
2020-08-11 16:39:13 +0530

    adds eslint configurations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React  from "react";
-import "./App.css";
-import {hot} from "react-hot-loader"
+import React from 'react';
+import './App.css';
+import { hot } from 'react-hot-loader';
 
-const App  = () => (
-    <div class="App">
-        <h1>Welcome to React Scratch!!!</h1>
-    </div>
+const App = () => (
+  <div className="App">
+    <h1>Welcome to React Scratch!!!</h1>
+  </div>
 );
 
 export default hot(module)(App);
